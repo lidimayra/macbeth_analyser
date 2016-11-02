473d740b2265f33bb64a471ca6d1d4402682db12
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-11-02 16:55:32 -0200

    Filter distinct speakers

--- lib/macbeth_analyser.rb
+++ lib/macbeth_analyser.rb
@@ -3,6 +3,16 @@ class MacbethAnalyser
 require 'nokogiri'
 require 'open-uri'
 
+  def speakers
+    speakers = Set.new
+
+    open_file.css("speaker").each do |speaker|
+      speakers.add(speaker.text) unless speaker.text == 'ALL'
+    end
+
+    speakers
+  end
+
   def open_file
     url = "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
     Nokogiri::HTML open(url)
--- spec/macbeth_analyser_spec.rb
+++ spec/macbeth_analyser_spec.rb
@@ -2,11 +2,18 @@ require 'rspec'
 require_relative '../lib/macbeth_analyser'
 
 describe 'Macbeth Analyser' do
-  subject(:macbeth_analyser) { MacbethAnalyser.new }
+  let(:macbeth_analyser) { MacbethAnalyser.new }
 
   context 'when opening Macbeth file' do
-    subject(:file) { macbeth_analyser.open_file }
+    subject { macbeth_analyser.open_file }
     it { is_expected.to be_a Nokogiri::XML::Document }
   end
 
+  context 'when reading speakers' do
+    subject(:speakers) { macbeth_analyser.speakers }
+
+    it 'returns distinct speakers' do
+      expect(speakers.count).to eq 40
+    end
+  end
 end
